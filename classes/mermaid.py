e9e7e0d1b1c023086cfc9abfd9e18a1fc5ab64e3
xm8925 <innocence510@gmail.com>
2022-04-06 14:42:01 -0700

    updated again -_-

--- classes/mermaid.py
+++ classes/mermaid.py
@@ -3,7 +3,7 @@ from character import Character
 
 class Mermaid(Character):
     def __init__( self , name, strength, speed, health):
-        super().__init__( self , name, strength, speed, health):
+        super().__init__(name, strength, speed, health)
         self.name = name
         self.strength = strength
         self.speed = speed
@@ -12,23 +12,34 @@ class Mermaid(Character):
     def show_stats( self ):
         print(f"Name: {self.name}\nStrength: {self.strength}\nSpeed: {self.speed}\nHealth: {self.health}\n")
 
-    def attack( self , mermaid ):
-        frenchie.health -= self.strength
-        bruiser.health -= self.strength
+    def attack( self , victim ):
+        victim.health -= self.strength
         print(self.strength)
         return self
     
-    def sea_call():
-        pass # adds strength
-    
-    def swim_fast():
-        if health is < 30
+    def sea_call(self):
+        if self.health < 20:
             self.health += 50
-            
-    def back_slap():
-        pass
-             
+        return self
     
-ceto = Mermaid("Ceto", strength=21, speed = 20, health = 5000)
+    def swim_fast(self):
+        if self.health < 10:
+            self.speed += 80
+        return self
 
-ceto.attack()
+            
+    def back_slap(self, victim):
+        if self.health <= 30:
+            self.strength += 40
+            victim.health -= self.strength
+        return self
+            
+
+    def Info(self):
+        print(f"The one the only {self.name}")
+        print(f"Health: {self.strength}")
+        print(f"Speed: {self.speed}")
+        print(f"Health: {self.health}")
+        return self
+    
+ceto = Mermaid("Ceto", strength=21, speed = 20, health = 100)
