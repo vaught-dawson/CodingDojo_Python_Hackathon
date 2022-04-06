d63e4dcbd51d93243b52eb6ac43c2f079f14bcdc
EthanRetzepis <Retzepise@gmail.com>
2022-04-06 13:59:42 -0700

    Added Bruiser

---
+++ classes/bruiser.py
@@ -0,0 +1,27 @@
+from character import Character
+
+class Bruiser(Character):
+    def __init__(self, name, strength, speed, health):
+        super().__init__(name, strength, speed, health)
+        self.name = name
+        self.strength = strength
+        self.speed = speed
+        self.health = health
+
+    def Info(self):
+        print(f"******  {self.name}  ******")
+        print(f"Health: {self.strength}")
+        print(f"Speed: {self.speed}")
+        print(f"Health: {self.health}")
+        return self
+
+
+p1 = Bruiser(name="Ethan",strength=24,speed=16,health=100)
+print(p1.show_stats())
+
+
+
+
+
+
+
